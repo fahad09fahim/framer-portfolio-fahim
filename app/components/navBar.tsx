59ff7ad8ee966a0db8b97d26d4e04117fc621174
fahim0509 <anik.fahim07@gmail.com>
2025-04-03 03:11:02 +0600

    navbar for large screen completed

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,17 +1,23 @@
+import { Link } from "react-router";
+
 export default function NavBar() {
   return (
     <nav>
-      <ul className="">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/project">Project</a>
-        </li>
-        <li>
-          <a href="/award">Award</a>
-        </li>
-      </ul>
+      <div className="flex justify-between items-center mx-auto p-[1.5rem]">
+        <div>
+          <Link to="/">Fahim</Link>
+        </div>
+        <div className="flex gap-4">
+          <Link to="/">Home</Link>
+          <Link to="/project">Project</Link>
+          <Link to="/award">Award</Link>
+        </div>
+        <div>
+          <button className="btn rounded-4xl bg-lime-500 bg-opacity-80 text-black px-4 py-2 ">
+            Contact Me
+          </button>
+        </div>
+      </div>
     </nav>
   );
 }
